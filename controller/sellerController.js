b7a8b2627c6642064e6b7ba70c6f9ed5e78518d8
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-05-16 18:16:53 +0700

    feat: completed seller crud feature

--- controller/sellerController.js
+++ controller/sellerController.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 
-let sellers = []
+var sellers = []
 
 const getSellerTest = (req, res) => {
   res.send('this is seller controller')
@@ -8,7 +8,6 @@ const getSellerTest = (req, res) => {
 
 const addSeller = (req, res) => {
   const {
-    id,
     shopName,
     address,
     skill,
@@ -16,10 +15,12 @@ const addSeller = (req, res) => {
     sellerName,
     phoneNumber,
     email,
-    priceRange,
   } = req.body
 
-  const insertedAt = new Date().toISOString();
+  const id = uuidv4()
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+  const priceRange = 10000
 
   const newSeller = {
     id,
@@ -31,12 +32,122 @@ const addSeller = (req, res) => {
     phoneNumber,
     email,
     priceRange,
+    insertedAt,
+    updatedAt
   }
 
   sellers.push(newSeller)
+
+  const isSuccess = sellers.filter((seller) => seller.id === id).length > 0
+
+  if (isSuccess) {
+    res.status(200)
+    res.send({
+      status: 'success',
+      message: 'Seller added successfully',
+      data: {
+        sellerId: newSeller.id
+      }
+    })
+  }
+
+  res.status(201).send('Seller failed to add')
+}
+
+const getAllSellers = (req, res) => {
+  if (sellers !== undefined) {
+    res.status(200).send(sellers)
+  }
+}
+
+const getSellerById = (req, res, h) => {
+  const { id } = req.params
+
+  const seller = sellers.filter(x => x.id === id)[0]
+
+  if (seller == undefined) {
+    res.status(404)
+    res.send({
+      status: 'success',
+      message: 'Seller not found'
+    })
+  }
+
+  res.status(200).send(seller)
+}
+
+const editSellerById = (req, res) => {
+  const { id } = req.params
+
+  const {
+    shopName,
+    address,
+    skill,
+    sellerPhoto,
+    sellerName,
+    phoneNumber,
+    email,
+  } = req.body
+
+  const updatedAt = new Date().toISOString()
+  const index = sellers.findIndex(seller => seller.id === id)
+  const priceRange = 10000
+
+  if (index !== -1) {
+    sellers[index] = {
+      ...sellers[index],
+      shopName,
+      address,
+      skill,
+      sellerPhoto,
+      sellerName,
+      phoneNumber,
+      email,
+      priceRange,
+      updatedAt
+    }
+
+    res.status(200)
+    res.send({
+      status: 'success',
+      message: 'Seller successfully updated',
+    })
+  }
+
+  res.status(404)
+  res.send({
+    status: 'fail',
+    message: 'Failed to update seller, id not found'
+  })
+}
+
+
+const deleteSellerById = (req, res) => {
+  const { id } = req.params
+
+  const index = sellers.findIndex(seller => seller.id === id)
+
+  if (index !== -1) {
+    sellers.splice(index, 1)
+    res.status(200)
+    res.send({
+      status: 'success',
+      message: 'Seller successfully deleted'
+    })
+  }
+
+  res.status(404)
+  res.send({
+    status: 'fail',
+    message: 'Seller failed to delete, id not found'
+  })
 }
 
 module.exports = {
   getSellerTest,
-  addSeller
+  addSeller,
+  getAllSellers,
+  getSellerById,
+  editSellerById,
+  deleteSellerById
 }
--- router/seller.js
+++ router/seller.js
@@ -1,17 +1,35 @@
 const express = require('express')
 const router = express.Router()
-// const getSellersTest = require('../controller/sellerController')
 const {
-  getSellerTest
+  getSellerTest,
+  addSeller,
+  getAllSellers,
+  getSellerById,
+  editSellerById,
+  deleteSellerById
 } = require('../controller/sellerController')
 
 // define the route
-router.get('/', (req, res) => {
+router.get('/default', (req, res) => {
   res.send('Sellers home page')
 })
 
 router.get('/test', getSellerTest)
 
+// start here
+router.post('/', addSeller)
+
+router.get('/', getAllSellers)
+
+router.get('/:id', getSellerById)
+
+router.get('/:id', getSellerById)
+
+router.put('/:id', editSellerById)
+
+router.delete('/:id', deleteSellerById)
+
+// example
 router.get('/about', (req, res) => {
   res.send('About birds')
 })
