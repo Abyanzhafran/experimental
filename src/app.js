5a67089aa94741631fb67a52ddfc3a6f6435a06d
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-05-24 22:04:00 +0700

    feat: add product router

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 3000
 const sellerRouter = require('../router/seller')
 const userRouter = require('../router/user')
 const searchRouter = require('../router/search')
+const productRouter = require('../router/product')
 
 app.use(express.json())
 app.use(cors())
@@ -14,6 +15,8 @@ app.use(cors())
 app.use('/seller', sellerRouter)
 app.use('/user', userRouter)
 app.use('/search', searchRouter)
+app.use('/product', productRouter)
+
 
 app.get('/', (req, res) => {
   res.send('Hello yoi mamen')
