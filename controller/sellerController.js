60b13e0feee497df949ea52d55d5446cea79dbb2
Abyanzhafran <zhafranjupleks5758@gmail.com>
2022-06-03 23:36:15 +0700

    feat: migrated to mysql sellerController function

--- controller/sellerController.js
+++ controller/sellerController.js
@@ -1,8 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 const db = require('../database')
 
-const sellers = []
-
 const getSellerTest = (req, res) => {
   res.send('this is seller controller')
 }
@@ -63,27 +61,30 @@ const addSeller = (req, res) => {
 }
 
 const getAllSellers = (req, res) => {
-  if (sellers !== undefined) {
-    res.status(200).send(sellers)
-  }
-
-  res.send('Cannot get seller')
+  let query = "SELECT * FROM tbl_seller"
+  db.query(query, (err, result) => {
+    if (err) {
+      res.status(404).send('Cannot get sellers')
+      throw err
+    }
+    res.status(200).send(result)
+  })
 }
 
 const getSellerById = (req, res, h) => {
   const { id } = req.params
 
-  const seller = sellers.filter(x => x.id === id)[0]
-
-  if (seller == undefined) {
-    res.status(404)
-    res.send({
-      status: 'fail',
-      message: 'Seller not found'
-    })
-  }
-
-  res.status(200).send(seller)
+  let query = "SELECT * FROM tbl_seller WHERE id = ?"
+  db.query(query, [id], (err, result) => {
+    if (result.length == 0) {
+      res.status(404).send({
+        status: 'fail',
+        message: 'Seller not found'
+      })
+    } else if (!err && result.length !== 0) {
+      res.status(200).send(result)
+    }
+  })
 }
 
 const editSellerById = (req, res) => {
@@ -105,58 +106,56 @@ const editSellerById = (req, res) => {
   } = req.body
 
   const updatedAt = new Date().toISOString()
-  const index = sellers.findIndex(seller => seller.id === id)
 
-  if (index !== -1) {
-    sellers[index] = {
-      ...sellers[index],
-      shopName,
-      province,
-      city,
-      streetName,
-      detailStreet,
-      skill,
-      sellerPhoto,
-      sellerName,
-      phoneNumber,
-      email,
-      updatedAt,
-      latitude,
-      longtitude
+  let query = "UPDATE tbl_seller SET shopName = ?, province = ?, city = ?, streetName = ?, detailStreet = ?, skill = ?, sellerPhoto = ?, sellerName = ?, phoneNumber = ?, email = ?, latitude = ?, longtitude = ?, updatedAt = ? WHERE id = ?"
+  db.query(query, [
+    shopName,
+    province,
+    city,
+    streetName,
+    detailStreet,
+    skill,
+    sellerPhoto,
+    sellerName,
+    phoneNumber,
+    email,
+    latitude,
+    longtitude,
+    updatedAt,
+    id
+  ], (err) => {
+    if (err) {
+      res.status(404)
+      res.send({
+        status: 'fail',
+        message: 'Seller not found'
+      })
+      throw err
     }
-
     res.status(200)
     res.send({
       status: 'success',
       message: 'Seller successfully updated'
     })
-  }
-
-  res.status(404)
-  res.send({
-    status: 'fail',
-    message: 'Failed to update seller, id not found'
   })
 }
 
 const deleteSellerById = (req, res) => {
   const { id } = req.params
 
-  const index = sellers.findIndex(seller => seller.id === id)
-
-  if (index !== -1) {
-    sellers.splice(index, 1)
-    res.status(200)
-    res.send({
+  let query = "DELETE FROM tbl_seller WHERE id = ?"
+  db.query(query, [id], (err, result) => {
+    if (err) {
+      res.status(404).send({
+        status: 'fail',
+        message: 'Seller failed to delete, id not found'
+      })
+      throw err
+    }
+    res.status(200).send({
       status: 'success',
       message: 'Seller successfully deleted'
     })
-  }
-
-  res.status(404)
-  res.send({
-    status: 'fail',
-    message: 'failed to delete seller, id not found'
   })
 }
 
